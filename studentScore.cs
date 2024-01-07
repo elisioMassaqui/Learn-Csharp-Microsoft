1ec31348ca2fc46647a95177adc7ed0d9453c1d8
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-07 12:38:16 +0000

    Para apresentar os nomes dos cursos juntamente com a nota numérica, introduza o seguinte código:

--- studentScore.cs
+++ studentScore.cs
@@ -65,3 +65,9 @@ int course3Grade = gradeB;
 int course4Grade = gradeB;
 int course5Grade = gradeA;
 
+Console.WriteLine($"{course1Name} {course1Grade}");
+Console.WriteLine($"{course2Name} {course2Grade}");
+Console.WriteLine($"{course3Name} {course3Grade}");
+Console.WriteLine($"{course4Name} {course4Grade}");
+Console.WriteLine($"{course5Name} {course5Grade}");
+
