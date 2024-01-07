570619ad31686843b7219322dfd25cad9bdd3b64
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-07 12:42:23 +0000

    horas de credito também

--- studentScore.cs
+++ studentScore.cs
@@ -50,11 +50,11 @@ string course3Name = "Biology 101";
 string course4Name = "Computer Science I";
 string course5Name = "Psychology 101";
 
-int course1credit = 3;
-int course2credit = 3;
-int course3credit = 4;
-int course4credit = 4;
-int course5credit = 3;
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
 
 int gradeA = 4;
 int gradeB = 3;
@@ -65,9 +65,9 @@ int course3Grade = gradeB;
 int course4Grade = gradeB;
 int course5Grade = gradeA;
 
-Console.WriteLine($"{course1Name} {course1Grade}");
-Console.WriteLine($"{course2Name} {course2Grade}");
-Console.WriteLine($"{course3Name} {course3Grade}");
-Console.WriteLine($"{course4Name} {course4Grade}");
-Console.WriteLine($"{course5Name} {course5Grade}");
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
 
